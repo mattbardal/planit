156f524ff0d647e8cf8b73b307b9c58a4423a739
mattbardal <mattbardal@gmail.com>
2022-07-31 11:05:14 -0600

    got basic authentication working

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/login'
-  get 'sessions/welcome'
-  get 'users/new'
-  get 'users/create'
+  resources :users, only: [:new, :create]
+
+  get 'login', action: :new, controller: :sessions
+  post 'login', action: :create, controller: :sessions
+
+  get 'logout', action: :logout, controller: :sessions
+
+  get 'welcome', action: :welcome, controller: :sessions
+
+  get 'authorized', action: :page_requires_login, controller: :sessions
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
